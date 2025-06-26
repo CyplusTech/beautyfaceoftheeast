a41cf57259109b76d5fab958b3757e44673ff032
Victor Umekwe <victorumekwe6@gmail.com>
2025-06-26 11:53:33 +0100

    Add start script to package.json

--- app.js
+++ app.js
@@ -24,10 +24,18 @@ const restrictionGateway = require("./middleware/gatekeeper");
 const app = express();
 
 // MongoDB Connection with Error Handling
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("✅ Connected to MongoDB"))
+//   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // Session Configuration
 app.use(
